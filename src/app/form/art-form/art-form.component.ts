4eb40ab44808869b127c87ba1cb168f136a0e6f8
Coltin Kifer <Coltin.Kifer@kennametal.com>
2018-06-18 13:27:29 -0400

    form validation, temp background

--- src/app/form/art-form/art-form.component.ts
+++ src/app/form/art-form/art-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ArtSubmission } from '../../model/art-submission-model';
 
 @Component({
@@ -10,6 +10,34 @@ import { ArtSubmission } from '../../model/art-submission-model';
 export class ArtFormComponent {
   model = new ArtSubmission();
   submitted = false;
+  form = new FormGroup({
+    firstname: new FormControl('', Validators.required),
+    lastname: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.pattern('[a-zA-z0-9_\.]+@[a-zA-Z]+\.[a-zA-Z]+')]),
+    congregation: new FormControl(),
+    file1: new FormControl('', Validators.required),
+    file2: new FormControl(),
+    file3: new FormControl(),
+    file4: new FormControl()
+  });
+
+  get firstname() {
+    return this.form.get('firstname');
+  }
+
+  get lastname() {
+    return this.form.get('lastname');
+  }
+
+  get email() {
+    return this.form.get('email');
+  }
+
+  get file1() {
+    return this.form.get('file1');
+  }
+
+
   onSubmit() {
      this.submitted = true;
   }
